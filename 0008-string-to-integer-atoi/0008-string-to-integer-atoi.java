5c8ae3a15c57299e03736eb4112559132508d052
Abhiwaghmare-07 <abhishekwaghmare701@gmail.com>
2026-09-07 18:56:58 +0530

    Time: 1 ms (100%), Space: 43.4 MB (97%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
@@ -1,53 +1,24 @@
 class Solution {
     public int myAtoi(String s) {
         int i=0;
-        int l=s.length();
-        while(i<l && Character.isWhitespace(s.charAt(i))){
-          i++;
-        }
-        if(i==l){
-            return 0;
-        }
-        char sign=s.charAt(i);
-        boolean negative=false;
-        if(sign=='+'){
+        int n=s.length();
+        while(i<n && s.charAt(i)==' '){
             i++;
         }
-        else if(sign=='-'){
-            negative=true;
+        int sign=1;
+        if(i<n && (s.charAt(i)=='+' || s.charAt(i)=='-')){
+            if(s.charAt(i)=='-') sign=-1;
             i++;
         }
-        else if(!Character.isDigit(s.charAt(i))){
-           return 0;
-        }
-         if(i==l){
-            return 0;
-        }
-         while(i<l && s.charAt(i)=='0'){
-          i++;
-        }
-          if(i==l){
-            return 0;
-        }
-        long value=0;
-        int max=Integer.MAX_VALUE/10;
-        int min=Integer.MIN_VALUE/10;
-
-        while(i<l && Character.isDigit(s.charAt(i))){
-
-             int digit=s.charAt(i)-'0';
-            if (value > max) {
-                return negative ? Integer.MIN_VALUE : Integer.MAX_VALUE;
-            } else if (value == max && digit > (negative ? 8 : 7)) {
-                return negative ? Integer.MIN_VALUE : Integer.MAX_VALUE;
+        int result=0;
+        while(i<n && Character.isDigit(s.charAt(i))){
+            int digit =s.charAt(i) - '0';
+               if (result > (Integer.MAX_VALUE - digit) / 10) {
+                return sign == 1 ? Integer.MAX_VALUE : Integer.MIN_VALUE;
             }
-
-             
-            
-                 value=value*10+(digit);
-           
+            result =result*10+digit;
             i++;
         }
-        return negative? (int)-value:(int)value;
+        return result*sign;
     }
 }
