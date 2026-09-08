10022d9764273454adc1bc0b72f7cebd3e9869d0
Abhiwaghmare-07 <abhishekwaghmare701@gmail.com>
2026-09-08 09:35:01 +0530

    Time: 1 ms (100%), Space: 43.6 MB (74.66%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
@@ -13,12 +13,23 @@ class Solution {
         int result=0;
         while(i<n && Character.isDigit(s.charAt(i))){
             int digit =s.charAt(i) - '0';
-               if (result > (Integer.MAX_VALUE - digit) / 10) {
-                return sign == 1 ? Integer.MAX_VALUE : Integer.MIN_VALUE;
+               
+            if (result > Integer.MAX_VALUE / 10 ||
+                (result == Integer.MAX_VALUE / 10 && digit > 7)) {
+
+                if (sign == 1) {
+                    return Integer.MAX_VALUE;
+                } else {
+                    return Integer.MIN_VALUE;
+                }
             }
+
             result =result*10+digit;
+            
             i++;
         }
-        return result*sign;
+         result = result * sign;
+         
+        return (int)result;
     }
 }
